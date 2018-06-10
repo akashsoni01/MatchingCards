3956ae20492478767eb9235782b029c104c286a8
Akash soni <akashsoni@Varun-Rathis-MacBook.local>
2018-06-10 20:02:38 +0530

    making file for Deck of cards also not included card for use till now.

---
+++ MatchingCards/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  MatchingCards
+//
+//  Created by Akash soni on 6/10/18.
+//  Copyright © 2018 Akash soni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
